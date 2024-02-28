37c39962812fe89228933eb20230653886a3c979
Jeremi Porras <jeremieliporrasangeles@gmail.com>
2024-02-28 16:30:34 -0500

    Cambios para desplegar en Railway

--- database/confing.js
+++ database/confing.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
     try {
 
-        await mongoose.connect(  process.env.DB_CNN/*
+        await mongoose.connect(  process.env.DB_CNN   /*
         #Con la nueva version de mongoose ya no hace falta especificar el objeto 
         como parametro en la funcion "connect" , com lo muextra el profesor en su clase.
         , {
